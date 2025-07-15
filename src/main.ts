53f40e51c50d167b36c327d967badaac935d84cf
Dr. Amr Osman <dr3mro@gmail.com>
2025-07-15 03:29:08 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app/app.module';
 
+dotenv.config();
 
 export async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let app: NestExpressApplication | NestFastifyApplication | null = null;
 
-  app.setGlobalPrefix('api/v2');
-  app.enableCors();
+  if (process.env.HTTP_ADAPTER === 'express') {
+    app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+      new ExpressAdapter(),
+    );
+  } else if (process.env.HTTP_ADAPTER === 'fastify') {
+    app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+  } else {
+    console.error('Please specify HTTP Adapter in env.');
+    process.exit(1); // early exit to avoid unsafe call
+  }
+
+  // Now TypeScript knows `app` is not null
+  app.setGlobalPrefix('api/v2').enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Valhalla 2.0 API')
@@ -17,13 +42,14 @@ export async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v2', app, document);
+  SwaggerModule.setup('/api/v2/', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 if (require.main === module) {
   bootstrap().catch((error) => {
     console.error('Error during application bootstrap:', error);
+    process.exit(1);
   });
 }
